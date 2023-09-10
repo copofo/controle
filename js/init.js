ae92e0d46ef59576096ad2f3c07a00abd7144e2d
copofo <121464011+copofo@users.noreply.github.com>
2023-09-10 00:06:24 -0300

    init

---
+++ js/init.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQHTo14OgEr8q56qLsIZC85CXIjcatYbM",
+    authDomain: "trio-ribeirao.firebaseapp.com",
+    projectId: "trio-ribeirao",
+    storageBucket: "trio-ribeirao.appspot.com",
+    messagingSenderId: "283549252712",
+    appId: "1:283549252712:web:6c73797dc8fa9320ef8fdd",
+    measurementId: "G-EBLKLCM893"
+  };
+
+  firebase.InitializeApp(firebaseConfig)
